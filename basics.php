011eb048665ec514e5865ace9c1f4b741faa9b78
UtkarshChoudhary19 <72205579+UtkarshChoudhary19@users.noreply.github.com>
2021-06-22 10:41:09 +0530

    First Commit

---
+++ basics.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+	<div class="container">
+		this is m first PHP website.
+		<?php
+			echo "hello world";
+			$var1=34;
+			$var2=55;
+			echo $var1;
+			echo $var2;
+		?>
+	</div>
+</body>
+</html>
